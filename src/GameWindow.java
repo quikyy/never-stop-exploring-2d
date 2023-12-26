56a7e487dc4734c5532cd45956779b6ba63a936e
quikyy <paczuskiszymon16@gmail.com>
2023-12-26 10:02:13 +0100

    windows, screens

--- src/GameWindow.java
+++ src/GameWindow.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class GameWindow {
+import javax.swing.*;
+
+public class GameWindow extends JFrame {
+
+    public GameWindow() {
+        this.setTitle("NeverStopExploring!");
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setResizable(false);
+
+        GameScreen gameScreen = new GameScreen();
+        this.add(gameScreen);
+
+        this.pack();
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+
+        gameScreen.run();
+    }
+
 }
--- src/PlayerCharacter.java
+++ src/PlayerCharacter.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class PlayerCharacter {
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class PlayerCharacter {
+
+    final int tileSize = 32;
+    int currentX = 64;
+    int currentY = 64;
+    final int initialSpeed = 100;
+
+
+    public void drawInitialPlayerCharacter(Graphics2D g2d) {
+        g2d.setColor(Color.WHITE);
+        g2d.fillRect(currentX, currentY, tileSize,tileSize);
+    }
+
 }
